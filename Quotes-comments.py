16091f262c0419e8a97aff8aa522660115ea940a
Doublex Nwankou <moudoblex@yahoo.com>
2026-06-10 23:54:20 +0100

    python quotes and comments lecture

---
+++ Quotes-comments.py
@@ -0,0 +1,24 @@
+# this is for single line comment
+
+"""
+This is for multiple line comment
+Double triple quotes
+Run the code
+"""
+
+'''
+This is also a multiline comment
+triple single quote
+run the code
+'''
+# Quotes in variable
+language = "english"
+print(language)
+print("language")
+print("""
+This a paragraph string,
+You can write as many lines as you want,
+third line
+""")
+print(this is a text) # this will error
+print("this is a text") # this will work
