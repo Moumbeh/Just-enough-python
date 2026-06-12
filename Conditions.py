e424735069e96d3c5685794d98b18913caa162d8
Doublex Nwankou <moudoblex@yahoo.com>
2026-06-12 08:19:30 +0100

    added conditional aguements

---
+++ Conditions.py
@@ -0,0 +1,18 @@
+# If condition
+x = 21
+
+if x < 30:
+    print("Inside the IF block")
+    print("X is less than 30")
+
+print("This is the rest of the code.")
+
+
+x = 35
+
+if x > 35:
+    print("X is greater than 35")
+elif x == 35:
+    print("X is equal to 35")
+else:
+    print("X is less than 35.")
